5253588cc565687fb291c33627b35b2f7a18d8b7
Mavera <81289423+Maveracim@users.noreply.github.com>
2022-09-02 11:51:35 +0300

    Update Moderation.Toplantı.js

--- Public Bots/app/commands/moderation/Moderation.Toplantı.js
+++ Public Bots/app/commands/moderation/Moderation.Toplantı.js
@@ -36,33 +36,3 @@ exports.mavera = {
     name:"toplantı",
     ek:["yoklama"]
 }
-
-/*const { MessageActionRow, MessageButton } = require("discord.js")
-const base = require("../../../settings/settings.json")
-const Setup = require("../../models/Setup")
-exports.run = async(client, msg, args, yes, no, embed, uye) => {
-    let db = await Setup.findOne({guild:msg.guild.id})
-    const row = new MessageActionRow().addComponents(new MessageButton().setCustomId("toplantıdağıt").setLabel("Mazeretli/Mazeretsiz Dağıt!").setStyle("DANGER"))
-    if(!msg.member.permissions.has(8n)) return
-    let rol = msg.guild.roles.cache.get(db.reg)
-    let mazeretli = msg.guild.members.cache.filter(x => x.roles.cache.has(db.mazeretli))
-    let sesteOlan = rol.members.filter(x => x.voice.channelId == db.toplantı && !x.user.bot)
-    let sesteOlmayan = rol.members.filter(x => x.presence && x.presence.status != "offline" && !x.user.bot && !x.roles.cache.has(db.mazeretli)).filter(x => !x.voice.channel || x.voice.channelId !== db.toplantı)
-    msg.channel.send({embeds:[embed.setDescription(`${msg.guild.emojis.cache.find(x => x.name == "mavera_blue")} Mazeretli rolünde \`${mazeretli.size}\` kişi bulunuyor. (\`${base.prefixes.moderation}mazeretli @Mavera/ID\`)
-${mazeretli.map(x => x).join(", ")?`${mazeretli.map(x => x).join(", ")}\n─────────────────────\n`:"\n"}
-${msg.guild.emojis.cache.find(x => x.name == "mavera_blue")} Çevrimiçi olup toplantı sesinde olmayan \`${sesteOlmayan.size}\` kişi bulunuyor. Kişiler:
-${sesteOlmayan.map(x => x).join(", ")?`\n${sesteOlmayan.map(x => x).join(", ")}─────────────────────\n`:"\n"}
-${msg.guild.emojis.cache.find(x => x.name == "mavera_settings")} Toplantı sesinde olan ve olmayan kullanıcılara rollerini vermek için butona tıkla!`)],components:[row]})
-        client.on("interactionCreate", async mavera => {
-            if(mavera.member.id !== msg.member.id) return
-            if(mavera.customId == "toplantıdağıt") {
-                sesteOlmayan.forEach(async x => await x.roles.add(db.katılmadı))
-                sesteOlan.forEach(async x => await x.roles.add(db.katıldı))
-                mavera.reply({content:`**${sesteOlmayan.size}** kişiye katılmadı, **${sesteOlan.size}** kişiye katıldı rolü veriliyor!`})
-            }
-        })
-}
-exports.mavera = {
-    name:"toplantı",
-    ek:["yoklama"]
-}*/
